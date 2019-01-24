212e783f52b0112d6c0177d50f8c4a8e7196f358
Marci Clark <marcig612@gmail.com>
2019-01-24 19:34:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,14 @@
-names = ["Jane Smith", "John Smith", "Susan Smith"]
+students = ["Jane Smith", "John Smith", "Susan Smith"]
 
 def my_collect(array)
-  i = 0 
-  names_collection = []
-  while i < array.length 
-    names_collection << yield(array[i])
-    
-    i += 0 
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection << yield(array[i])
+  
+    i += 1
   end
-  collection
+  name_collection
 end
 
-collection(names) {|i| i.split (" ").first}
+my_collect(students) {|i| i.split(" ").first}
