5bf0ef6cd509564a87d8a5f8d2a0c4b4ff70a7f4
Marci Clark <marcig612@gmail.com>
2019-01-24 19:28:15 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,9 @@ names = ["Jane Smith", "John Smith", "Susan Smith"]
 
 def my_collect(array)
   i = 0 
-  collection = []
+  names_collection = []
   while i < array.length 
-    collection << yield(array[i])
+    names_collection << yield(array[i])
     
     i += 0 
   end
